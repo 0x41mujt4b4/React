743f5d65f5265d906f8e17d8bc012b5c7bf220ee
Almujtaba007 <almujtaba007@gmail.com>
2021-12-08 17:11:07 +0200

    added styling

--- react_step_form/src/App.js
+++ react_step_form/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Submenu from "./components/Submenu/Submenu";
 import { UserForm } from './components/UserForm';
 
 const App = () => {
+  const [showQue, setShowQue] = useState(false)
+  const clickHandler = () => {
+    setShowQue(showQue => !showQue)
+  }
   return (
     <div className="App">
-      <UserForm />
+      <Navbar />
+      <Sidebar />
+      <Hero onClick={clickHandler}/>
+      <Submenu />
+      {showQue ? <UserForm /> : <></>}
     </div>
   );
 }
